afaa65e70fbc15be19a374727b55f106e268cb9f
Gaurav Singh <gaurav.singh323@gmail.com>
2022-06-28 18:50:31 +0530

    Add CSS for All Direction Hover

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
-import '../css/tooltip.css'
+import React, { Component } from "react";
+import "../css/tooltip.css";
 
 export default class Tooltip extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "my-tooltiptext t-left",
+      content : "Thanks for hovering!"
+    };
+  }
+  
   render() {
     return (
       <div class="my-tooltip">
-        <span class="my-tooltiptext">Thanks for Hover!</span>
+        <span class={this.state.input}>{this.state.content}</span>
         {this.props.children}
       </div>
-    )
+    );
   }
 }
